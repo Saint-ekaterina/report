6a561f6d588a0c08ab96a784ba753dda8881b206
Ekaterina <katena.ponomareva.01@inbox.ru>
2025-06-11 23:19:08 +0700

    correct errors

--- src/test/java/ru/netology/data/DataGenerator.java
+++ src/test/java/ru/netology/data/DataGenerator.java
@@ -17,19 +17,23 @@ public class DataGenerator {
 
     public static String generateDate(int days, String pattern) {
         String date = LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern(pattern));
+        // TODO: добавить логику для объявления переменной date и задания её значения, для генерации строки с датой
+        // Вы можете использовать класс LocalDate и его методы для получения и форматирования даты
         return date;
     }
 
     public static String generateCity() {
-
+//        админимтративные центры РФ
         String [] cities = new String[] {"Пермь", "Ижевск", "Екатеринбург", "Казань", "Оренбург",
                 "Самара", "Тольятти", "Новосибирск", "Барнаул", "Киров", "Тюмень"};
-
+//        // TODO: добавить логику для объявления переменной city и задания её значения, генерацию можно выполнить
+//        // с помощью Faker, либо используя массив валидных городов и класс Random
         return cities[new Random().nextInt(cities.length)];
 
 //        Faker faker = new Faker(Locale.forLanguageTag("ru"));
 //        String city = faker.address().city();
-//
+//        // TODO: добавить логику для объявления переменной city и задания её значения, генерацию можно выполнить
+//        // с помощью Faker, либо используя массив валидных городов и класс Random
 //        return city;
     }
 
@@ -38,12 +42,16 @@ public class DataGenerator {
         String lastName = faker.name().lastName();
         String firstName = faker.name().firstName();
         String name = (lastName + " " + firstName);
+        // TODO: добавить логику для объявления переменной name и задания её значения, для генерации можно
+        // использовать Faker
         return name;
     }
 
     public static String generatePhone() {
         Faker faker = new Faker(Locale.forLanguageTag("ru"));
         String phone = faker.phoneNumber().phoneNumber();
+        // TODO: добавить логику для объявления переменной phone и задания её значения, для генерации можно
+        // использовать Faker
         return phone;
     }
 
@@ -53,6 +61,8 @@ public class DataGenerator {
 
         public static UserInfo generateUser() {
             UserInfo user = new UserInfo(generateCity(), generateName(), generatePhone());
+            // TODO: добавить логику для создания пользователя user с использованием методов generateCity(locale),
+            // generateName(locale), generatePhone(locale)
             return user;
         }
     }
